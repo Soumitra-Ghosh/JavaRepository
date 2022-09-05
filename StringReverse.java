81d58664ede40d214ea5fa26e6ddf37a45f98e37
Soumitra-Ghosh <66515077+Soumitra-Ghosh@users.noreply.github.com>
2022-09-06 04:05:31 +0530

    Core Java

---
+++ StringReverse.java
@@ -0,0 +1,24 @@
+package JavaObject;
+
+import java.util.Scanner;
+
+public class StringReverse {
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String rev=" ";
+		
+		System.out.println("Enter name");
+		String name = sc.nextLine();
+		
+		char ch[]=name.toCharArray();
+		
+		for(int i=ch.length-1; i>=0; i--)
+		{
+			rev += ch[i];
+		}
+		
+		System.out.println(rev);
+	}
+}
