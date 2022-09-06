2aa50a3922dcadd82246fc1e6c09af599095209a
Soumitra-Ghosh <66515077+Soumitra-Ghosh@users.noreply.github.com>
2022-09-07 01:08:33 +0530

    Factorial
    Small factorial Program with a given number.

---
+++ Core Java/Factorial.java
@@ -0,0 +1,19 @@
+package JavaObject;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int factorial = 1;
+		
+		int num = 6 ; // 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
+		
+		for(int i= 1; i <= num; i ++)
+		{
+			factorial = factorial * i;
+		}
+		System.out.println("Factorial of " + num + " is "+ factorial);
+
+	}
+
+}
