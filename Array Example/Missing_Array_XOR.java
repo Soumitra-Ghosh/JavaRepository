9eb8846464de7d168062ebe58c71a9a0ab3c9cb7
Soumitra-Ghosh <66515077+Soumitra-Ghosh@users.noreply.github.com>
2022-09-08 22:40:00 +0530

    Missing Array in a list with XOR

---
+++ Array Example/Missing_Array_XOR.java
@@ -0,0 +1,25 @@
+package com.array.app;
+
+public class Missing_Array_XOR {
+
+	public static void main(String[] args) {
+		
+		int[] a = {1,2,3,5,6,7,8};
+		
+		int xor1 = a[0];
+		
+		for(int i=1; i<a.length; i++)
+		{
+			xor1 = xor1^a[i];
+		}
+		int xor2 = 1;
+		for(int i=2; i<=a.length+1; i++)
+		{
+			xor2 = xor2^i;
+		}
+		
+		System.out.println("Missing element : " + (xor1^xor2));
+
+	}
+
+}
