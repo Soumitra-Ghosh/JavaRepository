4f83be109004abae4b098f9897c0e23a66db7da5
Soumitra-Ghosh <66515077+Soumitra-Ghosh@users.noreply.github.com>
2022-09-08 22:36:55 +0530

    Array list equality

---
+++ Array Example/ArrayEqual.java
@@ -0,0 +1,19 @@
+package com.array.app;
+
+import java.util.Arrays;
+
+public class ArrayEqual {
+
+	public static void main(String[] args) {
+	
+		int[] arr1 = {1,5,8,3};
+		int[] arr2 = {3,6,4,8};
+		int[] arr3 = {4,7,1,3};
+		int[] arr4 = {4,7,1,3};
+		
+		System.out.println(Arrays.equals(arr3, arr4));
+
+		System.out.println(Arrays.equals(arr1, arr2));
+	}
+
+}
